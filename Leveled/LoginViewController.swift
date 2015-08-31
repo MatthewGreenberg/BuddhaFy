d5e323b60628e5b9f93d4c2cf84be52ef4aa3d8f
Andrew Carr <aacarr5@gmail.com>
2015-08-31 09:00:35 -0500

    Create login controller and modal

---
+++ Leveled/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Leveled
+//
+//  Created by Andrew  Carr on 8/31/15.
+//  Copyright © 2015 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
