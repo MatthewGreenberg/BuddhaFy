2f13bb78ee379d1d9d2845ce70419f506bcdb3f2
Andrew Carr <aacarr5@gmail.com>
2015-09-01 13:59:40 -0500

    Create persistant user data

--- Leveled/LoginViewController.swift
+++ Leveled/LoginViewController.swift
@@ -8,29 +8,22 @@
 
 import UIKit
 
+let kUserIDKey = "user_id"
+
 class LoginViewController: UIViewController {
     
-    
-    
     var userInfo : NSString = ""
-    
     
     @IBOutlet weak var usernameField: UITextField!
     
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var emailField: UITextField!
 
-    
-    
-    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-    
 
     }
-    
-    
     
     override func didReceiveMemoryWarning() {
         
@@ -39,21 +32,12 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
-    
-    
-    
-    
-    
-    
 
     @IBAction func signUpBttn() {
  
-        
         signUpUser()
         
     }
-    
-
     
     func signUpUser() {
         
@@ -65,54 +49,29 @@ class LoginViewController: UIViewController {
         
         request.HTTPMethod = "POST"
         
-        
-        
         let username = usernameField.text
         
         let password = passwordField.text
         
-        
-        
         let dataString = "user[username]=" + username! + "&" + "user[password]=" + password!
-        
-        
         
         let requestBodyData = (dataString as NSString).dataUsingEncoding(NSUTF8StringEncoding)
         
         request.HTTPBody = requestBodyData
         
-        
-        
         let queue:NSOperationQueue = NSOperationQueue()
-        
-        
         
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: {(response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
            
             let newData: NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
             
-            
-            
-            
             let stringArray = newData.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet)
             
             //            set core data
-            
-            
-            
-            print(stringArray[stringArray.count-2])
-            
-            
-            
-            
-            
+            var user_id = stringArray[stringArray.count-2]
+            NSUserDefaults.standardUserDefaults().setValue(user_id, forKey: kUserIDKey)
         })
-        
-        
-        
     }
-    
-    
     
     func getUserData() {
         
@@ -124,15 +83,9 @@ class LoginViewController: UIViewController {
         
         let request : NSMutableURLRequest = NSMutableURLRequest(URL: url)
         
-        
-        
         request.HTTPMethod = "GET"
-        
-        
-        
+
         let queue:NSOperationQueue = NSOperationQueue()
-        
-        
         
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{ (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
             
@@ -141,11 +94,4 @@ class LoginViewController: UIViewController {
         })
         
     }
-    
-    
-    
-    
-    
-    
-    
 }
